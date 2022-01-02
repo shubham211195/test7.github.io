755522cc829ed9f220425635187cf0d918729133
shubham211195 <93003514+shubham211195@users.noreply.github.com>
2022-01-02 19:18:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let no1=document.getElementById('x1');
+let no1=document.getElementById('no1');
 no1.addEventListener('click',function(){
    let flex=document.getElementById('no1')
    flex.classList.add('full');
@@ -6,5 +6,85 @@ no1.addEventListener('click',function(){
 let remove=document.getElementById('container')
 remove.addEventListener('click',function(){
     let remove_flex=document.getElementById('no1')
+     remove_flex.classList.remove('full');
+})
+let no2=document.getElementById('no2');
+no2.addEventListener('click',function(){
+   let flex=document.getElementById('no2')
+   flex.classList.add('full');
+})
+let remove1=document.getElementById('container')
+remove1.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no2')
+     remove_flex.classList.remove('full');
+})
+let no3=document.getElementById('no3');
+no3.addEventListener('click',function(){
+   let flex=document.getElementById('no3')
+   flex.classList.add('full');
+})
+let remove2=document.getElementById('container')
+remove2.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no3')
+     remove_flex.classList.remove('full');
+})
+let no4=document.getElementById('no4');
+no4.addEventListener('click',function(){
+   let flex=document.getElementById('no4')
+   flex.classList.add('full');
+})
+let remove3=document.getElementById('container')
+remove3.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no4')
+     remove_flex.classList.remove('full');
+})
+let no5=document.getElementById('no5');
+no5.addEventListener('click',function(){
+   let flex=document.getElementById('no5')
+   flex.classList.add('full');
+})
+let remove4=document.getElementById('container')
+remove4.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no5')
+     remove_flex.classList.remove('full');
+})
+let no6=document.getElementById('no6');
+no6.addEventListener('click',function(){
+   let flex=document.getElementById('no6')
+   flex.classList.add('full');
+})
+let remove5=document.getElementById('container')
+remove5.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no6')
+     remove_flex.classList.remove('full');
+})
+let no7=document.getElementById('no7');
+no7.addEventListener('click',function(){
+   let flex=document.getElementById('no7')
+   flex.classList.add('full');
+})
+let remove6=document.getElementById('container')
+remove6.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no7')
+     remove_flex.classList.remove('full');
+})
+let no8=document.getElementById('no8');
+no8.addEventListener('click',function(){
+   let flex=document.getElementById('no8')
+   flex.classList.add('full');
+})
+let remove7=document.getElementById('container')
+remove7.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no8')
+     remove_flex.classList.remove('full');
+})
+let no9=document.getElementById('no9');
+no9.addEventListener('click',function(){
+   let flex=document.getElementById('no9')
+   flex.classList.add('full');
+})
+let remove8=document.getElementById('container')
+remove8.addEventListener('click',function(){
+    let remove_flex=document.getElementById('no9')
      remove_flex.classList.remove('full');
 })
